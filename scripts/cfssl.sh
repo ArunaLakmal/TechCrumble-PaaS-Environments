d7d840964b2fa2b3caf14c9427a968cd76788b5e
Aruna Lakmal <arunarandunu@gmail.com>
2020-10-24 16:45:38 +0530

    update json

--- scripts/cfssl.sh
+++ scripts/cfssl.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-CERT_VERSION=$(jq -r '.cfssl_version' terraform.tfvars.json) ; \
-git clone https://github.com/ArunaLakmal/ansible-cfssl.git --branch $(jq -r '.cfssl_version' terraform.tfvars.json) --single-branch ; \
-ansible-playbook ansible-cfssl/cfssl-generate.yaml --extra-vars "@terraform.tfvars.json"
+
+create_certificates() {
+  CERT_VERSION=$(jq -r '.cfssl_version' terraform.tfvars.json) ; \
+  git clone https://github.com/ArunaLakmal/ansible-cfssl.git --branch $(jq -r '.cfssl_version' terraform.tfvars.json) --single-branch ; \
+  ansible-playbook ansible-cfssl/cfssl-generate.yaml --extra-vars "@terraform.tfvars.json"
+}
+
+if [[ -d "ansible-cfssl" ]]; then
+  rm -rf ansible-cfssl
+  create_certificates
+else
+  create_certificates
+fi
