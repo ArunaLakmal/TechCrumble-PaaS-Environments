0b6220546f64ab343632091d7c272bf45b063488
Aruna Lakmal <arunarandunu@gmail.com>
2020-10-24 18:13:36 +0530

    fixing ansible wrapper override

--- scripts/cfssl.sh
+++ scripts/cfssl.sh
@@ -2,7 +2,7 @@
 
 create_certificates() {
   CERT_VERSION=$(jq -r '.cfssl_version' terraform.tfvars.json) ; \
-  git clone https://github.com/ArunaLakmal/ansible-cfssl.git --branch $(jq -r '.cfssl_version' terraform.tfvars.json) --single-branch ; \
+  git clone https://github.com/ArunaLakmal/ansible-cfssl.git --branch $(jq -r '.ansible_cfssl_version' terraform.tfvars.json) --single-branch ; \
   ansible-playbook ansible-cfssl/cfssl-generate.yaml --extra-vars "@terraform.tfvars.json"
 }
 
